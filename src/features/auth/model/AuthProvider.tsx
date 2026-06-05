08c56e990d8edc46d4c21af2ef2067252b541520
Stephen Dang <nhan.dang.work@gmail.com>
2026-06-05 20:11:11 +0700

    feat: enhance AuthProvider to initialize user state from localStorage and improve login/logout functionality

--- src/features/auth/model/AuthProvider.tsx
+++ src/features/auth/model/AuthProvider.tsx
@@ -7,18 +7,28 @@ interface AuthProviderProps {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem("user");
+
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const login = () => {
-    setUser({
+    const user = {
       id: "1",
       name: "Nhan",
-      role: "admin",
-    });
+      role: "admin" as const,
+    };
+
+    setUser(user);
+
+    localStorage.setItem("user", JSON.stringify(user));
   };
 
   const logout = () => {
     setUser(null);
+
+    localStorage.removeItem("user");
   };
 
   return (
